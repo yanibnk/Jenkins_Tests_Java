3556be735f9ab65741cabc6f541e8ce0b1c00694
bstocker <boris.stocker@gmail.com>
2024-06-02 17:22:36 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,9 @@ class Main {
   public static void main(String[] args) {
      if(args[(args.length-1)].equals("+")){
         System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
-        System.exit(1);
       }
       else if(args[(args.length-1)].equals("-")){
         System.out.println(Integer.parseInt(args[0])-Integer.parseInt(args[1]));
-        System.exit(1);
       }
       else {System.exit(-1);}
     }
