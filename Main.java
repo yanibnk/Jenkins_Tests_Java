f45b51d48bb7c6dd4d5a0ef5aa8ef5a5896efd66
yanibnk <135028223+yanibnk@users.noreply.github.com>
2025-06-11 10:26:11 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,41 @@
 class Main {
   public static void main(String[] args) {
-     if(args[(args.length-1)].equals("+")){
-        System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
-      }
-      else if(args[(args.length-1)].equals("-")){
-        System.out.println(Integer.parseInt(args[0])-Integer.parseInt(args[1]));
-      }
-      else {System.exit(-1);}
+    if (args.length != 3) {
+      System.err.println("Erreur : 3 arguments attendus (int, int, opérateur)");
+      System.exit(-1);
     }
+
+    try {
+      int a = Integer.parseInt(args[0]);
+      int b = Integer.parseInt(args[1]);
+      String op = args[2];
+
+      switch (op) {
+        case "+":
+          System.out.println(a + b);
+          break;
+        case "-":
+          System.out.println(a - b);
+          break;
+        case "*":
+          System.out.println(a * b);
+          break;
+        case "/":
+          if (b != 0) {
+            System.out.println(a / b);
+          } else {
+            System.err.println("Erreur : division par zéro");
+            System.exit(-1);
+          }
+          break;
+        default:
+          System.err.println("Erreur : opérateur non reconnu");
+          System.exit(-1);
+      }
+
+    } catch (NumberFormatException e) {
+      System.err.println("Erreur : les deux premiers arguments doivent être des entiers");
+      System.exit(-1);
+    }
+  }
 }
